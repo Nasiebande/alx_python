fe06244fba89e11126b419d707e55d65a7ec85ba
Nasiebande <nikitaesther401@gmail.com>
2023-07-20 07:12:29 -0700

    4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fibonacci = [0, 1]
+    for i in range(2, n):
+        next_fib = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_fib)
+
+    return fibonacci[:n]

02d674c5e65573c5fb4806df45f2aa6489e8d19d
Nasiebande <nikitaesther401@gmail.com>
2023-07-20 06:53:10 -0700

    0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,6 +1,13 @@
-def add(a,b):
+def add(a, b):
+    # Perform addition using bitwise operations
     while b != 0:
         carry = a & b
         a = a ^ b
         b = carry << 1
+
     return a
+
+def add_with_negatives(a, b):
+    if b < 0:  # If b is negative, convert it to positive using bitwise negation
+        b = add(~b, 1)
+    return add(a, b)

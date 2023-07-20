c201388620527dd1c22961d3316fd72d492bef2f
Nasiebande <nikitaesther401@gmail.com>
2023-07-20 07:22:56 -0700

    6-password.py

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -3,10 +3,10 @@ def validate_password(password):
     if len(password) < 8:
         return False
 
-# Check if the password contains at least one uppercase letter, one lowercase letter, and one digit
-has_uppercase = any(char.isupper() for char in password)
-has_lowercase = any(char.islower() for char in password)
-has_digit = any(char.isdigit() for char in password)
+    # Check if the password contains at least one uppercase letter, one lowercase letter, and one digit
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
 
     if not (has_uppercase and has_lowercase and has_digit):
         return False

a23490bd344f1a04f795674768dad9df83d79669
Nasiebande <nikitaesther401@gmail.com>
2023-07-23 21:12:54 +0300

    Correction

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,24 @@
 import sys
 
-# Get the number of arguments passed (excluding the script name itself)
-num_arguments = len(sys.argv) - 1
+def main():
+    # Get the number of arguments passed (excluding the script name itself)
+    num_arguments = len(sys.argv) - 1
 
-# Print the number of arguments
-print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:", end=' ')
+    # Print the number of arguments
+    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:", end=' ')
 
-# Print the list of arguments
-if num_arguments == 0:
-    print(".", end='')
-else:
+    # Print the list of arguments
+    if num_arguments == 0:
+        print(".", end='')
+    else:
+        print()
+
+        # Print each argument with its position
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
+
+    # Print a new line
     print()
 
-    # Print each argument with its position
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
-
-# Print a new line
-print()
+if __name__ == "__main__":
+    main()

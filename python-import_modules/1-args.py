7bec222793509a9fe24d77be512830c3090024d7
Nasiebande <nikitaesther401@gmail.com>
2023-07-23 21:17:03 +0300

    Task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -14,8 +14,5 @@ def main():
         for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
 
-    # Print a new line
-    print()
-
 if __name__ == "__main__":
     main()

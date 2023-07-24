12681beb5f35a2a2f3d56f545edb558c972e2566
Nasiebande <nikitaesther401@gmail.com>
2023-07-24 15:13:28 +0300

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,21 +1,8 @@
+#!/usr/bin/python3
 a = 1
 b = 2
 
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
     return a + b
 
-if __name__ == "__main__":
-    # Calculate the result
-    result = add(a, b)
-
-    # Print the result using string formatting
-    print(f"{a} + {b} = {result}")
+print("{} + {} = {}".format(a, b, add(a, b)))

7f93879282df98a1d5c423a929c40d95807cef39
Nasiebande <nikitaesther401@gmail.com>
2023-07-24 15:23:16 +0300

    Task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,14 @@
+#!/usr/bin/python3
 import sys
 
-def main():
-    # Get the number of arguments passed (excluding the script name itself)
-    num_arguments = len(sys.argv) - 1
-
-    # Print the number of arguments
-    if num_arguments == 0:
-        print("0 arguments.")
-    else:
-        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
-        
-        # Print each argument with its position
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
-
 if __name__ == "__main__":
-    main()
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i, arg in enumerate(sys.argv[1:], 1):
+        print("{}: {}".format(i, arg))

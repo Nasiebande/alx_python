a258fd985c5f0f523038320b3b4e345441c8076a
Nasiebande <nikitaesther401@gmail.com>
2023-07-24 16:23:32 +0300

    Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,9 +1,12 @@
 def print_matrix_integer(matrix=[[]]):
+    if not matrix or not matrix[0]:
+        # Check if the matrix is empty or contains an empty row
+        print()
+        return
+
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
-                # Print the last element of the row without a trailing space
                 print("{:d}".format(row[i]))
             else:
-                # Print the element followed by a space
                 print("{:d}".format(row[i]), end=" ")

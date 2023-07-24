5e116d7336e8782360efd1aa6a61fe269a600d80
Nasiebande <nikitaesther401@gmail.com>
2023-07-24 16:47:42 +0300

    Task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,19 @@ def square_matrix_simple(matrix=[]):
     # Iterate through each element of the input matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            # Calculate the square value of the current integer and store it in the new matrix
+            # Calculate the square value of the current integer
             new_matrix[i][j] = matrix[i][j] ** 2
 
     return new_matrix
+
+# Test the function with the provided example
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)

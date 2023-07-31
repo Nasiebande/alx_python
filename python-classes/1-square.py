9a91af295623141357a407d60f3edb9c1c6b4233
Nasiebande <nikitaesther401@gmail.com>
2023-07-31 12:18:55 +0300

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,44 @@
+class Square:
+    """
+    Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the Square instance with the given size.
+
+        Parameters:
+            size (int, optional): The size of the square (default is 0).
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Parameters:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value

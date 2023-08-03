47c4950235e2f8f91183fa58dd8b1b002cd85314
Nasiebande <nikitaesther401@gmail.com>
2023-08-03 13:41:24 +0300

    Correction

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,9 +6,6 @@ class Square:
 
     Attributes:
         __size (int): The size of the square (private attribute).
-
-    Methods:
-        __init__(self, size): Initializes a new instance of the Square class.
     """
 
     def __init__(self, size):
@@ -19,3 +16,28 @@ class Square:
             size (int): The size of the square.
         """
         self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+# Test cases
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))  # Output: <class '__main__.Square'>
+    print(my_square.__dict__)  # Output: {'_Square__size': 3}
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)  # Output: 'Square' object has no attribute 'size'
+
+    try:
+        print(my_square._Square__size)
+    except Exception as e:
+        print(e)  # Output: 3

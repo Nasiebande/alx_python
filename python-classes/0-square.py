67066a4210b76cf0bc16564dffecdb008b47f45c
Nasiebande <nikitaesther401@gmail.com>
2023-08-03 13:48:50 +0300

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
+"""Square class definition"""
+
 
 class Square:
-    """
-    This class defines a square.
-
+    """Represents a square
     Attributes:
-        __size (int): The size of the square (private attribute).
+        __size (int): size of a side of the square
     """
-
     def __init__(self, size):
-        """
-        Initializes a new instance of the Square class.
-
+        """Initializes a square
         Args:
-            size (int): The size of the square.
+            size (int): size of a side of the square
+        Returns: None
         """
         self.__size = size
-
-    def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
-
-# Test cases
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))  # Output: <class '__main__.Square'>
-    print(my_square.__dict__)  # Output: {'_Square__size': 3}
-
-    try:
-        print(my_square.size)
-    except Exception as e:
-        print(e)  # Output: 'Square' object has no attribute 'size'
-
-    try:
-        print(my_square._Square__size)
-    except Exception as e:
-        print(e)  # Output: 3
+        

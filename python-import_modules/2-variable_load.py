b22986aae776cac1b6a5fd6ea2f85cc1bd817d7f
Nasiebande <nikitaesther401@gmail.com>
2023-08-03 05:03:13 -0700

    2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+from variable_load_2 import a
+
+# Print the value of the variable 'a'
+print(a)
 if __name__ == "__main__":
-    with open("variable_load_2.py", "r") as file:
-        contents = file.read()
-
-    variables = {}
-    exec(contents, variables)
-
-    if 'a' in variables:
-        print(variables['a'])
+    pass

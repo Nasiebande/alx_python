5bc2523e85dd7366bb7d477803513ac010e78ec9
Nasiebande <nikitaesther401@gmail.com>
2023-08-09 14:24:37 +0300

    Task 3

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and displays the response body or an error message"""
-
-import requests
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./4-error_code.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    response = requests.get(url)
 
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response.text)
+        print(r.text)

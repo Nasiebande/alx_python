051c004e068ac23a58cfce3d015cd2ac773ebb6c
Nasiebande <nikitaesther401@gmail.com>
2023-08-09 14:21:46 +0300

    Correction

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Sends a POST request to a URL with an email parameter and displays the response body"""
-
-import requests
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
+import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./2-post_email.py <URL> <email>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    payload = {"email": email}
-    response = requests.post(url, data=payload)
-
-    print(response.text)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
+        

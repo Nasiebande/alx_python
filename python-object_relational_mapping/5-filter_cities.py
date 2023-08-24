6fbbf70b48f28a6ef711bbf6dab89768d30c8e67
Nasiebande <nikitaesther401@gmail.com>
2023-08-24 08:11:06 -0700

    Task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Script to list cities of a specific state from the database hbtn_0e_4_usa
+"""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
@@ -9,44 +12,31 @@ if __name__ == "__main__":
               "<database name> <state name>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
 
-        # Create a cursor object to interact with the database
-        cursor = db.cursor()
+    cursor = db.cursor()
 
-        # Use parameterized query to retrieve cities of the specified state
-        query = ("SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ') "
-                 "FROM cities "
-                 "JOIN states ON cities.state_id = states.id "
-                 "WHERE states.name = %s")
-        cursor.execute(query, (state_name,))
+    query = ("SELECT cities.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "WHERE states.name = %s "
+             "ORDER BY cities.id ASC")
+    cursor.execute(query, (state_name,))
 
-        # Fetch the result
-        result = cursor.fetchone()[0]
+    cities = cursor.fetchall()
 
-        # Print the result
-        if result:
-            print(result)
-        else:
-            print("No cities found for the specified state.")
+    cursor.close()
+    db.close()
 
-        # Close the cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
+    if cities:
+        city_names = [city[0] for city in cities]
+        print(', '.join(city_names))
+    else:
+        print("")

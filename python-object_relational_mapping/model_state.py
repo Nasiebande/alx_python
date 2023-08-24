8aa99522bf0149b709e3e3c28a16cf9858d268ba
Nasiebande <nikitaesther401@gmail.com>
2023-08-24 07:38:06 -0700

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 class State(Base):
     """Class definition for the states table."""
     __tablename__ = 'states'

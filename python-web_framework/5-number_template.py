274ee8051ad7a54585a9db9b576f365e62623479
Nasiebande <nikitaesther401@gmail.com>
2023-08-31 07:09:24 -0700

    task 5

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,53 @@
+"""
+This is a script that starts a Flask Web application and also has
+an additional route/path.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+"""
+using @app.route to map "/" to the url
+"""
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+        return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+        
+@app.route('/c/<text>', strict_slashes=False)
+def custom_text(text):
+    modified_text = text.replace('_', ' ')
+    return "C {}".format(modified_text)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    modified_text = text.replace('_', ' ') if text else 'is cool'
+    return "Python {}".format(modified_text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+            Route handler for '/number/<n>'. Displays "<n> is a number" if n is an integer.
+
+            Args:
+                n (int): The number captured from the URL.
+    """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+            """
+            Route handler for '/number_template/<n>'. Displays an HTML page with "Number: n" in an H1 tag.
+
+            Args:
+                n (int): The number captured from the URL.
+            """
+            return render_template('number_template.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

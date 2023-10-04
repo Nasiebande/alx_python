b8b9b0ed072dc74a7a682d3a67c0e767a25f2686
Nasiebande <nikitaesther401@gmail.com>
2023-10-04 13:15:48 +0300

    Task 0 correction

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,46 +1,36 @@
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    # Define the base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
+def get_todo_progress(employee_id):
+    # Get employee details
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data["name"]
 
-    # Construct the URLs for employee details and TODO list
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/users/{employee_id}/todos"
+    # Get TODO list for the employee
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-    try:
-        # Fetch employee details
-        employee_response = requests.get(employee_url)
-        employee_response.raise_for_status()
-        employee_data = employee_response.json()
-        employee_name = employee_data["name"]
+    # Count completed tasks and total tasks
+    completed_tasks = [task for task in todos_data if task["completed"]]
+    total_tasks = len(todos_data)
+    num_completed_tasks = len(completed_tasks)
 
-        # Fetch TODO list for the employee
-        todos_response = requests.get(todos_url)
-        todos_response.raise_for_status()
-        todos_data = todos_response.json()
-
-        # Calculate the number of completed tasks
-        completed_tasks = [task for task in todos_data if task["completed"]]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(todos_data)
-
-        # Display the employee's TODO list progress
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-
-        # Display the titles of completed tasks
-        for task in completed_tasks:
-            print(f"    {task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+    # Print the result
+    print(
+        f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 script_name.py EMPLOYEE_ID")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+    try:
+        employee_id = int(sys.argv[1])
+        get_todo_progress(employee_id)
+    except ValueError:
+        print("Please provide a valid employee ID.")

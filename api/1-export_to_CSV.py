ba8e98111f2676b3a871d8c01b3185fa4ee9ef8a
Nasiebande <nikitaesther401@gmail.com>
2023-10-23 10:07:03 -0700

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,38 +2,17 @@ import csv
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        return
+user_id = str(sys.argv[1])
 
-    employee_id = sys.argv[1]
+user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
 
-    # Employee data
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_response = requests.get(employee_url)
-    if employee_response.status_code != 200:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-    employee_data = employee_response.json()
+user_data = requests.get(user_url).json()
+todo_data = requests.get(todo_url).json()
 
-    # Fetching todos data
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print(f"Failed to fetch todos for user {employee_data['name']}.")
-        return
-    todos_data = todos_response.json()
+filename = "{}.csv".format(user_id)
 
-    # Exporting to CSV file
-    csv_file = f"{employee_id}.csv"
-    with open(csv_file, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todos_data:
-            csv_writer.writerow([employee_id], employee_data['username'], task['completed'], task['title'])
-
-    print(f"Data exported to {csv_file}.")
-
-if __name__ == "__main":
-    main()
+with open(filename, 'w', newline='') as file:
+    writter = csv.writer(file, quoting = csv.QUOTE_ALL)
+    for task in todo_data:
+        writter.writerow([user_id, str(user_data['username']),task['completed'], task['title']])
